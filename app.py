40f36ae93ca8f030da35b237df8ba5c48707a191
Lucas Gonçalves Caldas <lucas.gcaldas@gmail.com>
2025-08-10 09:36:11 -0300

    Update app.py

--- app.py
+++ app.py
@@ -131,7 +131,7 @@ with col_graf4:
             media_ds_pais,
             locations='residencia_iso3',
             color='usd',
-            color_continuous_scale=["#F830E9", "#797AFE", "#12DDFA"],
+            color_continuous_scale=["#B8DFFF", "#9A9BFF", "#F830E9"],
             title='Salário médio de Cientista de Dados por país',
             labels={'usd': 'Salário médio (USD)', 'residencia_iso3': 'País'}
         )
